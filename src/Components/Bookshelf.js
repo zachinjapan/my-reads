d10895a0c48c076600b3248ece27a43290583082
Zach Stone <zachinjapan888s@gmail.com>
2021-10-25 10:06:38 -0400

    Created Book and Bookshelf Components and seperated full JSX file into appBackup.js

---
+++ src/Components/Bookshelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Book from "./Book";
+
+const Bookshelf = (props) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">Test</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          <Book />
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Bookshelf;
